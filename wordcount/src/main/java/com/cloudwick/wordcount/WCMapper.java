58d029ff8d385b0c6f4c127597da6f087e02bad9
bharath-sankurath <bharath.sankurath@cloudwick.com>
2014-02-05 13:29:11 -0800

    Feb 5-1:30pm commit

---
+++ wordcount/src/main/java/com/cloudwick/wordcount/WCMapper.java
@@ -0,0 +1,33 @@
+package com.cloudwick.wordcount;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WCMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+	/* (non-Javadoc)
+	 * @see org.apache.hadoop.mapreduce.Mapper#map(java.lang.Object, java.lang.Object, org.apache.hadoop.mapreduce.Mapper.Context)
+	 */
+	@Override
+	protected void map(LongWritable key, Text value, Context context)
+			throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		String[]  words= value.toString().split(",");
+		for(String word:words)
+		{
+			if(word.length()>0)
+			{
+				context.write(new Text(word),new IntWritable(1));
+			}
+		}
+		
+		
+	}
+	
+
+	
+}
